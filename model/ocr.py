638d47624b0cd26827f12ec7400847fd68f433cc
Nikolay Pavlychev <nikolaypavlychev@yandex.ru>
2022-10-09 20:27:28 +0300

    OCR module

--- model/ocr.py
+++ model/ocr.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -9,7 +10,7 @@ import easyocr
 ROOT_DIR = os.path.abspath(os.curdir)
 print(ROOT_DIR)
 
-# TODO: обернуть в класс Model, OCR будет на вход получать Frame.number, а зполнять frame.recognise_number
+
 def show_image(image_path):
     image = mpimg.imread(image_path)
     plt.imshow(image, cmap="gray")
@@ -17,17 +18,26 @@ def show_image(image_path):
 
 def detection_recognize(image_path):
 
-    reader = easyocr.Reader(["ru"])
+    reader = easyocr.Reader(["ru"],gpu=True)
     result = reader.readtext(image_path, detail=0)
-    if len(result) == 1:
-        print("auto number:", result[0][:8])
-        return result[0][:8]
-    if len(result) == 3:
-        print("auto number:", result[0][:6] + result[2])
-        return result[0][:6] + result[2]
-    else:
-        print("Don't recognize")
-        return None
+    result = result[0].upper()
+    result_rec = []
+
+    for i, character in enumerate(result):
+        try:
+            ch = re.match(u"[А-Яа-яA-Za-z]",character)[0]
+            if ch=='О':
+                ch='0'
+            if int(i)<=5:
+                result_rec.append(ch)
+        except TypeError:
+            try:
+                ch = re.match(r"\d",character)[0]
+                result_rec.append(ch)
+            except TypeError:
+                continue
+    
+    return ''.join(result_rec)
 
 
 if __name__ == "__main__":
@@ -37,4 +47,8 @@ if __name__ == "__main__":
     print(detection_recognize(image_path))
 
 
-# python3 ocr.py /home/nikolaypavlychev/ITMO_DL_1/autoriaNumberplateOcrRu/test/img/A001BP54.png
+# python3 ocr.py /home/nikolaypavlychev/ITMO_DL_1/car_tracking/ITMO_DL_1/autoriaNumberplateOcrRu/test/img/A001BP54.png
+# /home/nikolaypavlychev/ITMO_DL_1/car_tracking/ITMO_DL_1/itmo_dl_env/bin/python -m  ./model/ocr.py /home/nikolaypavlychev/ITMO_DL_1/car_tracking/ITMO_DL_1/autoriaNumberplateOcrRu/test/img/A001PC71.png
+# image_path = '/home/pavlychev/ITMO_DL_1/car_tracking/ITMO_DL_1/autoriaNumberplateOcrRu/test/img/A001PC71.png'
+# print(detection_recognize(image_path))
+
---
+++ model/ocr_model.py
@@ -0,0 +1,60 @@
+import os
+import glob
+import re
+import sys
+from tqdm import tqdm
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+current_dir=os.getcwd()
+sys.path.append(current_dir)
+
+from model import ocr
+from model.ocr import detection_recognize
+import importlib
+importlib.reload(ocr)
+
+
+letters = {
+        u'A': u'А',
+        u'B' : u'В',
+        u'C' : u'С',
+        u'E' : u'Е',
+        u'H': u'Н',
+        u'K': u'К',
+        u'M': u'М',
+        u'O' : u'О',
+        u'P' : u'Р',
+        u'T' : u'Т',
+        u'Y' : u'У',
+        u'X' : u'Х'      
+    }
+
+
+class Model(object):
+    def __init__(self,image_path):
+        self.image_path = image_path
+
+    def recognize(self,image_path):
+        recognize_list = []
+        recognize = detection_recognize(image_path)
+        recognize_post = []
+        for i, ch in enumerate(recognize):
+            if (ch=='О')&(i in [1,2,3]):
+                recognize_post.append('0')
+            if (ch=='0')&(i in [0]):
+                recognize_post.append('О')
+            else:
+                recognize_post.append(ch)
+            recognize_post_str = ''.join(recognize_post)
+
+        recognize_list.append(recognize_post_str)
+
+        return recognize_list[0]
+
+obj = Model(image_path)
+obj.recognize(image_path)
+
+
+
